a1a8137eb76e7501c60c9362c8ec5138c32fb21d
Masonio822 <masonelzy@gmail.com>
2024-05-10 07:49:09 -0400

    Initial commit

---
+++ src/main/java/net/masonio/mosquitos/MosquitosClient.java
@@ -0,0 +1,10 @@
+package net.masonio.mosquitos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MosquitosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
