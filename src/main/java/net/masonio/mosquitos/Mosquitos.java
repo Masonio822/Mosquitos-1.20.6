06ff1f56cf67464e143f3b4ee16af02d6cc9a09e
Masonio822 <masonelzy@gmail.com>
2024-05-18 10:01:47 -0400

    Enchantment still needs fixing

--- src/main/java/net/masonio/mosquitos/Mosquitos.java
+++ src/main/java/net/masonio/mosquitos/Mosquitos.java
@@ -1,9 +1,15 @@
 package net.masonio.mosquitos;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.masonio.mosquitos.enchantment.LeechEnchantment;
 import net.masonio.mosquitos.item.ModItems;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -19,5 +25,13 @@ public class Mosquitos implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		Registry.register(Registries.ENCHANTMENT, new Identifier(MOD_ID, "leech"), LEECH);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+			ItemStack itemAfter = new ItemStack(Items.ENCHANTED_BOOK);
+			itemAfter.addEnchantment(Enchantments.PIERCING, 4);
+			ItemStack leechBook = new ItemStack(Items.ENCHANTED_BOOK);
+			leechBook.addEnchantment(LEECH, 1);
+			content.addAfter(itemAfter, leechBook);
+				});
 	}
 }
--- src/main/java/net/masonio/mosquitos/item/ModItems.java
+++ src/main/java/net/masonio/mosquitos/item/ModItems.java
@@ -6,6 +6,7 @@ import net.masonio.mosquitos.Mosquitos;
 import net.masonio.mosquitos.item.custom.MosquitoSackItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,7 +16,7 @@ public class ModItems {
     public static final Item REDMOSQUITOSAC = registerItem("red_mosquito_sack", new MosquitoSackItem(new Item.Settings().maxDamage(21)));
 
     private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries){
-        entries.add(REDMOSQUITOSAC);
+        entries.addAfter(Items.TOTEM_OF_UNDYING, REDMOSQUITOSAC);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -24,6 +25,6 @@ public class ModItems {
 
     public static void registerModItems() {
         Mosquitos.LOGGER.info("Registering Mod Items for " + Mosquitos.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToIngredientTabItemGroup);
     }
 }
